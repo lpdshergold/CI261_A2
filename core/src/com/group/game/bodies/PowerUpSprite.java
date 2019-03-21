97891fb781dbaa92188886cdd28bacb7bdc51480
lpdshergold <L.Shergold1@uni.brighton.ac.uk>
2019-03-21 16:50:11 +0000

    Created the PowerUpSprite class - need to complete BonusManager

--- core/src/com/group/game/bodies/PowerUpSprite.java
+++ core/src/com/group/game/bodies/PowerUpSprite.java
@@ -3,20 +3,54 @@ package com.group.game.bodies;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.group.game.utility.TweenData;
+import com.group.game.utility.UniversalResource;
 
+import aurelienribon.tweenengine.BaseTween;
 import aurelienribon.tweenengine.TweenCallback;
 import aurelienribon.tweenengine.TweenManager;
 
 public class PowerUpSprite extends AnimatedSprite {
     private TweenData tweenData;
     private TweenManager tweenManager;
+    private TweenCallback callback;
     private float animationTime;
     private int ttl;
     private float timeCount;
-    private boolean display = false;
-    private TweenCallback callback;
+    private boolean displayed = false;
+
+    public boolean isDisplay() {return displayed;}
 
     public PowerUpSprite(String atlasString, Texture t, Vector2 pos) {
         super(atlasString, t, pos);
+        // Alpha set a 0 means the sprite cannot be seen
+        this.setAlpha(0);
+        ttl = setTimeToLive(0);
+        callback = new TweenCallback() {
+            @Override
+            public void onEvent(int type, BaseTween<?> source) {
+                setDisplayed(false);
+                initTweenData();
+                BonusManager.handlingCollision = false;
+            }
+        };
+        initTweenData();
     }
+
+    private void initTweenData() {
+        tweenData = new TweenData();
+        tweenData.setXy(new Vector2 (this.getX(), this.getY()));
+        tweenData.setColor(this.getColor());
+        tweenData.setScale(this.getScaleX());
+        tweenManager = UniversalResource.getInstance().tweenManager; //tweenManager
+    }
+
+    private int setTimeToLive(int t) {
+        ttl = t;
+        return ttl;
+    }
+
+    private void setDisplayed(boolean d) {
+        displayed = d;
+    }
+
 }
